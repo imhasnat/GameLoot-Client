dcf68a5ce0baa658237deaaee1cde9f92c61223e
Hasnat Mahbub <imhasnat00@gmail.com>
2022-11-24 16:18:56 +0600

    firebase config, authcontext, publicroute added

---
+++ src/Contexts/AuthProvider/AuthProvider.js
@@ -0,0 +1,61 @@
+import React, { createContext, useEffect, useState } from 'react';
+import { createUserWithEmailAndPassword, getAuth, onAuthStateChanged, signInWithEmailAndPassword, signOut, updateProfile, signInWithPopup } from 'firebase/auth';
+import app from '../../firebase/firebase.config';
+
+const auth = getAuth(app);
+export const AuthContext = createContext();
+
+const AuthProvider = ({ children }) => {
+    const [user, setUser] = useState(null)
+    const [loading, setLoading] = useState(true);
+
+    useEffect(() => {
+        const unsubscribe = onAuthStateChanged(auth, currentUser => {
+            setLoading(false);
+            setUser(currentUser);
+        })
+
+        return () => unsubscribe();
+    }, [])
+
+    const createUser = (email, password) => {
+        setLoading(true);
+        return createUserWithEmailAndPassword(auth, email, password)
+    }
+
+    const updateUser = userinfo => {
+        return updateProfile(auth.currentUser, userinfo)
+    }
+
+    const login = (email, password) => {
+        setLoading(true);
+        return signInWithEmailAndPassword(auth, email, password);
+    }
+
+    const loginPopup = provider => {
+        setLoading(true);
+        return signInWithPopup(auth, provider);
+    }
+
+    const logout = () => {
+        return signOut(auth.currentUser);
+    }
+
+    const value = {
+        user,
+        loading,
+        setLoading,
+        createUser,
+        updateUser,
+        login,
+        loginPopup,
+        logout,
+    }
+    return (
+        <AuthContext.Provider value={value}>
+            {children}
+        </AuthContext.Provider>
+    );
+};
+
+export default AuthProvider;
---
+++ src/Routes/PublicRoutes/PublicRoutes.js
@@ -0,0 +1,28 @@
+import { createBrowserRouter } from "react-router-dom";
+import Main from "../../Layout/Main";
+import ErrorPage from "../../Pages/ErrorPage/ErrorPage";
+import Login from "../../Pages/Login/Login";
+import SignUp from "../../Pages/SignUp/SignUp";
+
+export const router = createBrowserRouter([
+    {
+        path: '/',
+        element: <Main></Main>,
+        errorElement: <ErrorPage></ErrorPage>,
+        children: [
+            {
+                path: '/',
+                element: <Login></Login>
+            },
+            {
+                path: '/login',
+                element: <Login></Login>
+            },
+            {
+                path: '/signup',
+                element: <SignUp></SignUp>
+            }
+
+        ]
+    }
+])
