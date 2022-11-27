455c18bef48fefcfe033d8cbd4fab21147143c44
Hasnat Mahbub <imhasnat00@gmail.com>
2022-11-27 16:32:51 +0600

    advertise show on the homepage implemented

---
+++ src/Pages/Home/Advertise/Advertise.js
@@ -0,0 +1,27 @@
+import { useQuery } from '@tanstack/react-query';
+import React from 'react';
+
+const Advertise = () => {
+
+    const { data: advertises = [] } = useQuery({
+        queryKey: ['advertises'],
+        queryFn: async () => {
+            const res = await fetch(`${process.env.REACT_APP_Server_URL}/product/advertise`)
+            const data = await res.json();
+            console.log(data);
+            return data;
+        }
+    })
+
+    return (
+        <div className={`${advertises.length ? 'block' : 'hidden'}`}>
+            <h1>advertise</h1>
+            {
+                advertises.map(advertise =>
+                    <p>{advertise.title}</p>)
+            }
+        </div>
+    );
+};
+
+export default Advertise;
